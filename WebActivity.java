b76b74ee9ca024f3e54235065b1e59f3fde82414
Sunny Gupta <vknow360@gmail.com>
2020-07-23 16:34:42 +0530

    Update WebActivity.java

--- WebActivity.java
+++ WebActivity.java
@@ -4,6 +4,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import java.util.List;
+import java.util.ArrayList;
+import com.google.appinventor.components.runtime.util.JsonUtil;
 public class WebActivity extends Activity{
 	@Override
     protected void onCreate(Bundle saved){
@@ -11,11 +14,14 @@ public class WebActivity extends Activity{
         if (getIntent() != null){
             Uri uri = getIntent().getData();
             if (uri != null){
-							PackageManager packageManager = getPackageManager();
-        			Intent intent = packageManager.getLaunchIntentForPackage(getPackageName());
-							intent.putExtra("APP_INVENTOR_START",'"'+uri.toString()+'"');
-        			startActivity(intent);
-							finish();
+            	List<String> startValue = new ArrayList<>();
+          		startValue.add(uri.toString());
+          		startValue.add("1");
+				PackageManager packageManager = getPackageManager();
+        		Intent intent = packageManager.getLaunchIntentForPackage(getPackageName());
+				intent.putExtra("APP_INVENTOR_START", JsonUtil.getJsonRepresentation(startValue));
+        		startActivity(intent);
+				finish();
             }
         }
     }
