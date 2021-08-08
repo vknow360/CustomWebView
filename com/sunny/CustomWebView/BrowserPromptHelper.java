3a6507404c0dd0905a7dbed17f361290551ddab6
Sunny Gupta <vknow360@gmail.com>
2021-08-08 17:22:18 +0530

    Update BrowserPromptHelper.java

--- com/sunny/CustomWebView/BrowserPromptHelper.java
+++ com/sunny/CustomWebView/BrowserPromptHelper.java
@@ -1,29 +1,30 @@
 package com.sunny.CustomWebView;
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import com.google.appinventor.components.annotations.androidmanifest.*;
 import com.google.appinventor.components.runtime.*;
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 @DesignerComponent(version = 1,
-        versionName = "1.1",
+        versionName = "1.2",
         description ="Helper class of CustomWebView extension to add app to browsers list<br> Developed by Sunny Gupta",
         category = ComponentCategory.EXTENSION,
         nonVisible = true,
         iconName = "https://res.cloudinary.com/andromedaviewflyvipul/image/upload/c_scale,h_20,w_20/v1571472765/ktvu4bapylsvnykoyhdm.png",
         helpUrl="https://github.com/vknow360/CustomWebView",
         androidMinSdk = 21)
-@UsesActivities(activities = {@ActivityElement(intentFilters = {@IntentFilterElement(actionElements = {@ActionElement(name = "android.intent.action.VIEW")}, categoryElements = {@CategoryElement(name = "android.intent.category.DEFAULT"), @CategoryElement(name = "android.intent.category.BROWSABLE")}, dataElements = {@DataElement(scheme = "http"), @DataElement(scheme = "https")}), @IntentFilterElement(actionElements = {@ActionElement(name = "android.intent.action.VIEW")}, categoryElements = {@CategoryElement(name = "android.intent.category.DEFAULT"), @CategoryElement(name = "android.intent.category.BROWSABLE")}, dataElements = {@DataElement(scheme = "http"), @DataElement(scheme = "https"), @DataElement(mimeType = "text/html"), @DataElement(mimeType = "text/plain"), @DataElement(mimeType = "application/xhtml+xml")})},name="appinventor.ai_vknow360.CustomWebView.Screen1",launchMode = "singleTask")})
+@UsesActivities(activities = {@ActivityElement(intentFilters = {@IntentFilterElement(actionElements = {@ActionElement(name = "android.intent.action.VIEW")}, categoryElements = {@CategoryElement(name = "android.intent.category.DEFAULT"), @CategoryElement(name = "android.intent.category.BROWSABLE")}, dataElements = {@DataElement(scheme = "http"), @DataElement(scheme = "https")}), @IntentFilterElement(actionElements = {@ActionElement(name = "android.intent.action.VIEW")}, categoryElements = {@CategoryElement(name = "android.intent.category.DEFAULT"), @CategoryElement(name = "android.intent.category.BROWSABLE")}, dataElements = {@DataElement(scheme = "http"), @DataElement(scheme = "https"), @DataElement(mimeType = "text/html"), @DataElement(mimeType = "text/plain"), @DataElement(mimeType = "application/xhtml+xml")})},name="appinventor.ai_vknow360.CustomWebView.Screen1",launchMode = "singleTop")})
 @SimpleObject(external=true)
-public class BrowserPromptHelper extends AndroidNonvisibleComponent implements OnResumeListener{
+public class BrowserPromptHelper extends AndroidNonvisibleComponent implements OnNewIntentListener {
     public Activity activity;
     public BrowserPromptHelper(ComponentContainer container){
         super(container.$form());
         activity = container.$context();
-        form.registerForOnResume(this);
+        form.registerForOnNewIntent(this);
     }
-    public String getUrl(){
-        Uri uri = activity.getIntent().getData();
+    public String getUrl(Intent intent){
+        Uri uri = intent.getData();
         if (uri != null && uri.toString() != null){
             return uri.toString();
         }
@@ -31,15 +32,16 @@ public class BrowserPromptHelper extends AndroidNonvisibleComponent implements O
     }
     @SimpleFunction(description = "Returns the url which started the current activity")
     public String GetStartUrl(){
-        return getUrl();
+        return getUrl(activity.getIntent());
     }
-
-    @Override
-    public void onResume() {
-        OnResume(getUrl());
-    }
+    
     @SimpleEvent(description = "Event raised when app gets resumed and gives the url which started this activity/screen if there is any else empty string")
     public void OnResume(String url){
         EventDispatcher.dispatchEvent(this,"OnResume",url);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        OnResume(getUrl(intent));
     }
 }
